5f2161521ec4d8d192c111e6358628a653439bb4
Xaocer <xaocerlolerstone02815@gmail.com>
2024-10-22 19:12:08 +1000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import goh from "./assets/ghhbnjjj.jpg"
+import Button from './components/Ui/Buttons/Button'
+
+function App() {
+
+
+  return (
+    <>
+      <img src={goh} alt="" />
+      <Button text="buy"/>
+       <Button text="sell"/>
+        <Button text="kill"/>
+         <Button text="revive"/>
+          <Button text="bring"/>
+          <Button text="name"/>
+          <Button text="win"/>
+          <Button text="lost"/>
+          <Button text="hit"/>
+          <Button text="jump"/>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Ui/Buttons/Button.jsx
@@ -0,0 +1,7 @@
+import "./style.css"
+function Button(props){
+return <button className="button">
+    {props.text}
+</button>
+}
+export default Button
